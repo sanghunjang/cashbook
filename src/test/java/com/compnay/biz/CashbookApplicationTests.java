c424047265830b613e724d5954613b3e97912f99
SangHun <tkdgns0703@naver.com>
2023-05-24 15:46:35 +0900

    v 1.0.0

---
+++ src/test/java/com/compnay/biz/CashbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compnay.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
